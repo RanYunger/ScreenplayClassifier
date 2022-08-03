3d87cc847adeb2a0ad0fa4ddfa80881355c55019
RanYunger <RanYunger123>
2022-08-03 10:22:48 +0300

    ReportsView design

--- ScreenplayClassifier/MVVM/ViewModels/ReportsViewModel.cs
+++ ScreenplayClassifier/MVVM/ViewModels/ReportsViewModel.cs
@@ -1,6 +1,8 @@
-﻿using ScreenplayClassifier.MVVM.Views;
+﻿using ScreenplayClassifier.MVVM.Models;
+using ScreenplayClassifier.MVVM.Views;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Text;
 
@@ -9,17 +11,51 @@ namespace ScreenplayClassifier.MVVM.ViewModels
     class ReportsViewModel : INotifyPropertyChanged
     {
         // Fields
+        private ObservableCollection<ClassificationModel> classificationReports;
         public event PropertyChangedEventHandler PropertyChanged;
 
         // Properties
         public MainViewModel MainViewModel { get; private set; }
         public ReportsView ReportsView { get; private set; }
 
+        public ObservableCollection<ClassificationModel> ClassificationReports
+        {
+            get { return classificationReports; }
+            set
+            {
+                classificationReports = value;
+
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("ClassificationReports"));
+            }
+        }
+
         // Constructors
-        public ReportsViewModel() { }
+        public ReportsViewModel()
+        {
+            ClassificationReports = new ObservableCollection<ClassificationModel>();
+        }
 
         // Methods
         #region Commands
+        public Command FilterReportsCommand
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                });
+            }
+        }
+        public Command ClearFilterCommand
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                });
+            }
+        }
         #endregion
 
         public void Init(ReportsView reportsView, MainViewModel mainViewModel)
