4275bf86310f26f91eff9bf107530ce8dac22e46
RanYunger <62587988+RanYunger@users.noreply.github.com>
2022-09-03 12:12:32 +0300

    HomeView changes

--- ScreenplayClassifier/MVVM/ViewModels/HomeViewModel.cs
+++ ScreenplayClassifier/MVVM/ViewModels/HomeViewModel.cs
@@ -18,28 +18,156 @@ namespace ScreenplayClassifier.MVVM.ViewModels
     public class HomeViewModel : INotifyPropertyChanged
     {
         // Fields
+        private ImageSource leftImage, centerImage, rightImage;
+        private string leftModule, centerModule, rightModule, moduleTooltip;
         public event PropertyChangedEventHandler PropertyChanged;
 
         // Properties
         public MainViewModel MainViewModel { get; private set; }
         public HomeView HomeView { get; private set; }
 
+        public ImageSource LeftImage
+        {
+            get { return leftImage; }
+            set
+            {
+                leftImage = value;
+
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("LeftImage"));
+            }
+        }
+
+        public ImageSource CenterImage
+        {
+            get { return centerImage; }
+            set
+            {
+                centerImage = value;
+
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("CenterImage"));
+            }
+        }
+
+        public ImageSource RightImage
+        {
+            get { return rightImage; }
+            set
+            {
+                rightImage = value;
+
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("RightImage"));
+            }
+        }
+
+        public string LeftModule
+        {
+            get { return leftModule; }
+            set
+            {
+                leftModule = value;
+
+                LeftImage = new BitmapImage(new Uri(FolderPaths.IMAGES + leftModule + "Disabled.png"));
+
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("LeftModule"));
+            }
+        }
+
+        public string CenterModule
+        {
+            get { return centerModule; }
+            set
+            {
+                centerModule = value;
+
+                switch (centerModule)
+                {
+                    case "Archives": ModuleTooltip = "View classifications categorized by genre"; break;
+                    case "Classification": ModuleTooltip = "Classify screenplays to genres"; break;
+                    case "Reports": ModuleTooltip = "View classification reports"; break;
+                }
+
+                CenterImage = new BitmapImage(new Uri(FolderPaths.IMAGES + centerModule + "Enabled.png"));
+
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("CenterModule"));
+            }
+        }
+
+        public string RightModule
+        {
+            get { return rightModule; }
+            set
+            {
+                rightModule = value;
+
+                RightImage = new BitmapImage(new Uri(FolderPaths.IMAGES + rightModule + "Disabled.png"));
+
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("RightModule"));
+            }
+        }
+
+        public string ModuleTooltip
+        {
+            get { return moduleTooltip; }
+            set
+            {
+                moduleTooltip = value;
+
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("ModuleTooltip"));
+            }
+        }
+
         // Constructors
         public HomeViewModel() { }
 
         // Methods
         #region Commands   
-        public Command ShowArchivesViewCommand
+        public Command CheckKeyCommand
         {
-            get { return new Command(() => MainViewModel.ShowView(MainViewModel.ArchivesView)); }
+            get
+            {
+                return new Command(() =>
+                {
+                    string temp = string.Empty;
+
+                    if (Keyboard.IsKeyDown(Key.Left))
+                    {
+                        temp = LeftModule;
+                        LeftModule = CenterModule;
+                        CenterModule = RightModule;
+                        RightModule = temp;
+                    }
+                    else if (Keyboard.IsKeyDown(Key.Right))
+                    {
+                        temp = RightModule;
+                        RightModule = CenterModule;
+                        CenterModule = LeftModule;
+                        LeftModule = temp;
+                    }
+                });
+            }
         }
-        public Command ShowClassificationViewCommand
+
+        public Command ShowModuleViewCommand
         {
-            get { return new Command(() => MainViewModel.ShowView(MainViewModel.ClassificationView)); }
-        }
-        public Command ShowReportsViewCommand
-        {
-            get { return new Command(() => MainViewModel.ShowView(MainViewModel.ReportsView)); }
+            get
+            {
+                return new Command(() =>
+                {
+                    switch (CenterModule)
+                    {
+                        case "Archives": MainViewModel.ShowView(MainViewModel.ArchivesView); break;
+                        case "Classification": MainViewModel.ShowView(MainViewModel.ClassificationView); break;
+                        case "Reports": MainViewModel.ShowView(MainViewModel.ReportsView); break;
+                    }
+                });
+            }
         }
         #endregion
 
@@ -47,6 +175,10 @@ namespace ScreenplayClassifier.MVVM.ViewModels
         {
             MainViewModel = mainViewModel;
             HomeView = homeView;
+
+            LeftModule = "Archives";
+            CenterModule = "Classification";
+            RightModule = "Reports";
         }
     }
 }
