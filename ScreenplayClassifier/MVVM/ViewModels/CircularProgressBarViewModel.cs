e6cde5103a6006b835dcf587b93384d9a88c860b
RanYunger <62587988+RanYunger@users.noreply.github.com>
2022-10-06 11:48:49 +0300

    CircularProgressBar design

---
+++ ScreenplayClassifier/MVVM/ViewModels/CircularProgressBarViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScreenplayClassifier.MVVM.ViewModels
+{
+    public class CircularProgressBarViewModel
+    {
+        // Fields
+
+        // Properties
+
+        // Constructors
+
+        // Methods
+        #region Commands
+        public Command ProgressCompleteCommand
+        {
+            get
+            {
+                return new Command(() =>
+                {
+
+                });
+            }
+        }
+        #endregion
+    }
+}
