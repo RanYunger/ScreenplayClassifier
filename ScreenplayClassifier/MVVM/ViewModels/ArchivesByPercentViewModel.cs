2cba178a318e6135f10aa19c288d21c276f0cc86
RanYunger <62587988+RanYunger@users.noreply.github.com>
2022-11-17 11:59:56 +0200

    ArchivesByPercentViewModel advancements

--- ScreenplayClassifier/MVVM/ViewModels/ArchivesByPercentViewModel.cs
+++ ScreenplayClassifier/MVVM/ViewModels/ArchivesByPercentViewModel.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Text;
+using System.Windows.Controls;
 using System.Windows.Data;
 
 namespace ScreenplayClassifier.MVVM.ViewModels
@@ -17,11 +18,13 @@ namespace ScreenplayClassifier.MVVM.ViewModels
     {
         // Fields
         private List<string> allGenres;
-        private Predicate<object> genreFilter;
-        private Predicate<object> subGenre1Filter;
-        private Predicate<object> subGenre2Filter;
+        private Predicate<object> genreFilter, genrePercentageFilter;
+        private Predicate<object> subGenre1Filter, subGenre1PercentageFilter;
+        private Predicate<object> subGenre2Filter, subGenre2PercentageFilter;
 
         private string filteredGenre, filteredSubGenre1, filteredSubGenre2;
+        private float filteredGenrePercentage, filteredSubGenre1Percentage, filteredSubGenre2Percentage;
+        private float filteredGenreProximity, filteredSubGenre1Proximity, filteredSubGenre2Proximity;
         private ScreenplayModel selectedScreenplay;
         private ObservableCollection<ScreenplayModel> archives;
         private ObservableCollection<string> genreOptions, subGenre1Options, subGenre2Options;
@@ -114,6 +117,78 @@ namespace ScreenplayClassifier.MVVM.ViewModels
             }
         }
 
+        public float FilteredGenrePercentage
+        {
+            get { return filteredGenrePercentage; }
+            set
+            {
+                filteredGenrePercentage = value;
+
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("FilteredGenrePercentage"));
+            }
+        }
+
+        public float FilteredSubGenre1Percentage
+        {
+            get { return filteredSubGenre1Percentage; }
+            set
+            {
+                filteredSubGenre1Percentage = value;
+
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("FilteredSubGenre1Percentage"));
+            }
+        }
+
+        public float FilteredSubGenre2Percentage
+        {
+            get { return filteredSubGenre2Percentage; }
+            set
+            {
+                filteredSubGenre2Percentage = value;
+
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("FilteredSubGenre2Percentage"));
+            }
+        }
+
+        public float FilteredGenreProximity
+        {
+            get { return filteredGenreProximity; }
+            set
+            {
+                filteredGenreProximity = value;
+
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("FilteredGenreProximity"));
+            }
+        }
+
+        public float FilteredSubGenre1Proximity
+        {
+            get { return filteredSubGenre1Proximity; }
+            set
+            {
+                filteredSubGenre1Proximity = value;
+
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("FilteredSubGenre1Proximity"));
+            }
+        }
+
+        public float FilteredSubGenre2Proximity
+        {
+            get { return filteredSubGenre2Proximity; }
+            set
+            {
+                filteredSubGenre2Proximity = value;
+
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("FilteredSubGenre2Proximity"));
+            }
+        }
+
         public ScreenplayModel SelectedScreenplay
         {
             get { return selectedScreenplay; }
@@ -190,7 +265,7 @@ namespace ScreenplayClassifier.MVVM.ViewModels
             {
                 return new Command(() =>
                 {
-                    ICollectionView screenplaysCollectionView = CollectionViewSource.GetDefaultView(ArchivesViewModel.Screenplays);
+                    ICollectionView screenplaysCollectionView = CollectionViewSource.GetDefaultView(Archives);
 
                     genreFilter = (o) =>
                     {
@@ -205,9 +280,38 @@ namespace ScreenplayClassifier.MVVM.ViewModels
                         return string.IsNullOrEmpty(FilteredSubGenre2) ? true : ((ScreenplayModel)o).ActualSubGenre2 == FilteredSubGenre2;
                     };
 
+                    genrePercentageFilter = (o) =>
+                    {
+                        ScreenplayModel screenplay = (ScreenplayModel)o;
+                        float genrePercentage = screenplay.MatchingPercentages[screenplay.ActualGenre];
+                        bool lowerBoundaryCondition = genrePercentage >= (FilteredGenrePercentage - FilteredGenreProximity),
+                            upperBoundaryCondition = genrePercentage <= (FilteredGenrePercentage + FilteredGenreProximity);
+
+                        return string.IsNullOrEmpty(FilteredGenre) ? true : (lowerBoundaryCondition) && (upperBoundaryCondition);
+                    };
+                    subGenre1PercentageFilter = (o) =>
+                    {
+                        ScreenplayModel screenplay = (ScreenplayModel)o;
+                        float subGenre1Percentage = screenplay.MatchingPercentages[screenplay.ActualSubGenre1];
+                        bool lowerBoundaryCondition = subGenre1Percentage >= (FilteredSubGenre1Percentage - FilteredSubGenre1Proximity),
+                            upperBoundaryCondition = subGenre1Percentage <= (FilteredSubGenre1Percentage + FilteredSubGenre1Proximity);
+
+                        return string.IsNullOrEmpty(FilteredSubGenre1) ? true : (lowerBoundaryCondition) && (upperBoundaryCondition);
+                    };
+                    subGenre2PercentageFilter = (o) =>
+                    {
+                        ScreenplayModel screenplay = (ScreenplayModel)o;
+                        float subGenre1Percentage = screenplay.MatchingPercentages[screenplay.ActualSubGenre2];
+                        bool lowerBoundaryCondition = subGenre1Percentage >= (FilteredSubGenre2Percentage - FilteredSubGenre2Proximity),
+                            upperBoundaryCondition = subGenre1Percentage <= (FilteredSubGenre2Percentage + FilteredSubGenre2Proximity);
+
+                        return string.IsNullOrEmpty(FilteredSubGenre2) ? true : (lowerBoundaryCondition) && (upperBoundaryCondition);
+                    };
+
                     screenplaysCollectionView.Filter = (o) =>
                     {
-                        return (genreFilter.Invoke(o)) && (subGenre1Filter.Invoke(o)) && (subGenre2Filter.Invoke(o));
+                        return (genreFilter.Invoke(o)) && (subGenre1Filter.Invoke(o)) && (subGenre2Filter.Invoke(o)) &&
+                            (genrePercentageFilter.Invoke(o)) && (subGenre1PercentageFilter.Invoke(o)) && (subGenre2PercentageFilter.Invoke(o));
                     };
                     screenplaysCollectionView.Refresh();
 
@@ -229,6 +333,14 @@ namespace ScreenplayClassifier.MVVM.ViewModels
                     FilteredGenre = null;
                     FilteredSubGenre1 = null;
                     FilteredSubGenre2 = null;
+
+                    FilteredGenrePercentage = 50f;
+                    FilteredSubGenre1Percentage = 50f;
+                    FilteredSubGenre2Percentage = 50f;
+
+                    FilteredGenreProximity = 1f;
+                    FilteredSubGenre1Proximity = 1f;
+                    FilteredSubGenre2Proximity = 1f;
 
                     FilterReportsCommand.Execute(null);
                 });
