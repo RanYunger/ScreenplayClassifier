b8dfbb0e89375a1d937764db01cf593e76f6c45c
RanYunger <62587988+RanYunger@users.noreply.github.com>
2023-01-11 18:46:47 +0200

    Credits in AboutView

--- ScreenplayClassifier/MVVM/ViewModels/AboutViewModel.cs
+++ ScreenplayClassifier/MVVM/ViewModels/AboutViewModel.cs
@@ -13,25 +13,12 @@ namespace ScreenplayClassifier.MVVM.ViewModels
     public class AboutViewModel : INotifyPropertyChanged
     {
         // Fields
-        private Timer videoTimer;
         private bool isPlaying;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
         // Properties
         public AboutView AboutView { get; private set; }
-
-        public Timer VideoTimer
-        {
-            get { return videoTimer; }
-            set
-            {
-                videoTimer = value;
-
-                if (PropertyChanged != null)
-                    PropertyChanged(this, new PropertyChangedEventArgs("VideoTimer"));
-            }
-        }
 
         public bool IsPlaying
         {
@@ -40,10 +27,10 @@ namespace ScreenplayClassifier.MVVM.ViewModels
             {
                 isPlaying = value;
 
-                //if (isPlaying)
-                //    PlayVideoCommand.Execute(null);
-                //else
-                //    InterruptVideoCommand.Execute(null);
+                if (isPlaying)
+                    PlayVideoCommand.Execute(null);
+                else
+                    InterruptVideoCommand.Execute(null);
 
                 if (PropertyChanged != null)
                     PropertyChanged(this, new PropertyChangedEventArgs("IsPlaying"));
@@ -55,39 +42,28 @@ namespace ScreenplayClassifier.MVVM.ViewModels
 
         // Methods
         #region Commands
-        public Command ScreamCommand
-        {
-            get
-            {
-                return new Command(() =>
-                {
-                    MediaPlayer mediaPlayer = new MediaPlayer();
-
-                    mediaPlayer.Open(new Uri(FolderPaths.AUDIOS + "Wilhelm Scream.mp3"));
-                    mediaPlayer.Play();
-                });
-            }
-        }
-
         public Command PlayVideoCommand
         {
             get
             {
-                MediaElement curtainsMediaElement = (MediaElement)AboutView.FindName("CurtainsMediaElement");
+                MediaElement mediaElement = (MediaElement)AboutView.FindName("MediaElement");
 
                 return new Command(() =>
                 {
-                    curtainsMediaElement.Source = new Uri(FolderPaths.VIDEOS + "Curtains.mp4");
-                    curtainsMediaElement.Play();
-
-                    videoTimer.Start();
+                    mediaElement.Source = new Uri(FolderPaths.VIDEOS + "Credits.mp4");
+                    mediaElement.Play();
                 });
             }
         }
 
         public Command InterruptVideoCommand
         {
-            get { return new Command(() => VideoTimer.Stop()); }
+            get
+            {
+                MediaElement mediaElement = (MediaElement)AboutView.FindName("MediaElement");
+
+                return new Command(() => mediaElement.Stop());
+            }
         }
         #endregion
 
@@ -98,20 +74,6 @@ namespace ScreenplayClassifier.MVVM.ViewModels
         public void Init(AboutView aboutView)
         {
             AboutView = aboutView;
-
-            VideoTimer = new Timer(6000);
-            VideoTimer.Elapsed += VideoTimer_Elapsed;
-        }
-
-        /// <summary>
-        /// Callback method to the timer's Elapsed event.
-        /// </summary>
-        /// <param name="sender">The invoker of the event</param>
-        /// <param name="e">The event argument</param>
-        private void VideoTimer_Elapsed(object sender, ElapsedEventArgs e)
-        {
-            VideoTimer.Stop();
-            IsPlaying = false;
         }
     }
 }
