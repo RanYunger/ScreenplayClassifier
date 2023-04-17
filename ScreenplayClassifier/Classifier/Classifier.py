faf9f0eb310b4e2372b276ad0d2f696dc74d2c48
RanYunger <62587988+RanYunger@users.noreply.github.com>
2023-04-17 08:10:17 +0300

    Dummy model restored [FOR NOW]

--- ScreenplayClassifier/Classifier/Classifier.py
+++ ScreenplayClassifier/Classifier/Classifier.py
@@ -1,81 +1,47 @@
 # Imports
-import time
-import numpy
-import pandas
-import pickle
+import os, time, pandas, pickle
 
-from sklearn.ensemble import VotingClassifier
-
-import Constants
-from Loader import load_test_screenplays
-from ScreenplayProcessor import protagonist_roles_dict, time_periods, times_of_day
-from sklearn.metrics import accuracy_score
-from sklearn.multioutput import MultiOutputClassifier
-from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split, GridSearchCV
+from pathlib import Path
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+from sklearn.multiclass import OneVsRestClassifier
 from sklearn.preprocessing import MultiLabelBinarizer
 
+from Loader import *
+from TextProcessor import *
+
+# Globals
+default_binarizer   = MultiLabelBinarizer()
+default_vectorizer  = TfidfVectorizer(max_df=0.8, max_features=10000)
+default_classifier  = OneVsRestClassifier(LogisticRegression())
+
 # Methods
-def save_model(model):
-    # Writes model variables to pickle file
-    pickle_file = open(Constants.model_pickle_path, "wb")
-    pickle.dump(model, pickle_file)
+def load_pickle():
+    pickle_file = open(Path.cwd() / "Classifier/Pickle", "rb")
+    loaded_binarizer, loaded_vectorizer, loaded_classifier = pickle.load(pickle_file)
     pickle_file.close()
 
-def encode(screenplays):
-    # Encodes the textual values in the screenplays dataframe
-    protagonist_roles = list(protagonist_roles_dict.values())
+    binarizer = loaded_binarizer if loaded_vectorizer is not None else default_binarizer
+    vectorizer = loaded_vectorizer if loaded_vectorizer is not None else default_vectorizer
+    classifier = loaded_classifier if loaded_classifier is not None else default_classifier
 
-    screenplays.replace({"Protagonist Roles": {role: protagonist_roles.index(role) for role in protagonist_roles},
-                         "Time Period": {period: time_periods.index(period) for period in time_periods},
-                         "Dominant Time of Day": {time: times_of_day.index(time) for time in times_of_day}},
-                        inplace=True)
+    return [binarizer, vectorizer, classifier]
 
-    return screenplays
+def save_pickle(binarizer, vectorizer, classifier):
+    # Validation
+    saved_binarizer = binarizer if binarizer is not None else default_binarizer
+    saved_vectorizer = vectorizer if vectorizer is not None else default_vectorizer
+    saved_classifier = classifier if classifier is not None else default_classifier
 
-def get_optimal_amount_of_estimators(x, t, base_estimator):
-    hyper_parameters = {"n_estimators": list(range(10, 21)), "bootstrap": [True, False]}
-    gridSearch = GridSearchCV(VotingClassifier(base_estimator=base_estimator, random_state=42), hyper_parameters,
-                              scoring="neg_log_loss").fit(x, t)
-
-    return gridSearch.best_params_["n_estimators"], gridSearch.best_params_["bootstrap"]
-
-def create_model():
-    # Creates a classification model
-    train_screenplays = encode(pandas.read_csv(Constants.train_csv_path))
-    train_screenplays["Genres"] = [eval(genres) for genres in train_screenplays["Genres"]]
-
-    t = MultiLabelBinarizer().fit_transform(train_screenplays["Genres"])
-    x = train_screenplays.drop(["Title", "Genres"], axis=1)
-    x_train, x_validation, t_train, t_validation = train_test_split(x, t, test_size=0.2, random_state=42)
-
-    # Builds classifier and predicts its accuracy score (current best: SVC - 0.1441)
-    base_estimator = MultiOutputClassifier(SVC(probability=True)).fit(x_train, t_train)
-    classifier = base_estimator
-
-    t_predictions = classifier.predict(x_validation)
-    score = accuracy_score(t_validation, t_predictions)
-    print("Accuracy: {:.4f}".format(score))
-
-    # Saves model variables to file
-    save_model(classifier)
-
-    return classifier
-
-def load_model():
-    # Validates existence of pickle file
-    if not Constants.model_pickle_path.exists():
-        return create_model()
-
-    # Reads model variables from pickle file
-    pickle_file = open(Constants.model_pickle_path, "rb")
-    model = pickle.load(pickle_file)
+    pickle_file = open(Path.cwd() / "Classifier/Pickle", "wb")
+    pickle.dump([saved_binarizer, saved_vectorizer, saved_classifier], pickle_file)
     pickle_file.close()
-
-    return model
 
 def probabilities_to_percentages(probabilities):
-    probabilities_dict = dict(zip(Constants.genre_labels, probabilities))
+    genre_labels = json.load(open("Jsons/Genres.json"))
+    probabilities_dict = dict(zip(genre_labels, probabilities))
     probabilities_dict = dict(sorted(probabilities_dict.items(), key=lambda item: item[1], reverse=True))
     sum_of_probabilities = sum(probabilities)
     percentages_dict = {}
@@ -86,25 +52,49 @@ def probabilities_to_percentages(probabilities):
 
     return percentages_dict
 
-def classify(file_paths):
-    # Loads test screenplays and classification model
-    test_screenplays = encode(load_test_screenplays(file_paths))
-    classifier = load_model()
+def train():
+    # Loads classifier's variables from file
+    binarizer, vectorizer, classifier = load_pickle()
+
+    # If training is required
+    if (binarizer == default_binarizer) or (vectorizer == default_vectorizer) or (classifier == default_classifier):
+        train_screenplays = pandas.merge(read_train_screenplays(), read_genres(), on="Title")
+
+        binarizer.fit(train_screenplays["Actual Genres"])
+        y = binarizer.transform(train_screenplays["Actual Genres"])
+
+        x_train, x_validation, y_train, y_validation = train_test_split(train_screenplays["Text"], y, test_size=0.2,
+                                                                        random_state=42)
+
+        x_train_tfidf = vectorizer.fit_transform(x_train)
+        x_validation_tfidf = vectorizer.transform(x_validation)
+
+        classifier.fit(x_train_tfidf, y_train)
+
+        y_probabilities = classifier.predict_proba(x_validation_tfidf)
+
+    return [binarizer, vectorizer, classifier]
+
+def classify(classifier_variables, file_paths):
+    test_screenplays = read_test_screenplays(file_paths)
+    binarizer, vectorizer, classifier = classifier_variables
     classifications_dict = {}
     classifications_complete = 0
 
-    # Classifies the test screenplays
-    for offset, screenplay in test_screenplays.iterrows():
-        features_vector = [feature[0] for feature in numpy.array(screenplay[1:]).reshape(-1, 1)]
-        genre_probabilities = [probability.tolist()[0] for probability in classifier.predict_proba([features_vector])]
-        genre_probabilities = [probability[0] for probability in genre_probabilities]
-        classifications_dict[file_paths[offset]] = probabilities_to_percentages(genre_probabilities)
+    for offset, test_screenplay in test_screenplays.iterrows():
+        test_vector = vectorizer.transform([test_screenplay["Text"]])
+        test_probabilities = sum(classifier.predict_proba(test_vector).tolist(), []) # Flattens the list
+        test_percentages = probabilities_to_percentages(test_probabilities)
 
-        # Prints progress (for GUI to update progress)
+        classifications_dict[file_paths[offset]] = test_percentages
+
         classifications_complete += 1
         print(classifications_complete)
 
-        time.sleep(0.5)  # seconds
+        time.sleep(0.5) # Sleep for 0.5 seconds
+
+    # Saves classifier's variables to file
+    save_pickle(binarizer, vectorizer, classifier)
 
     return pandas.DataFrame({"FilePath": classifications_dict.keys(),
                              "GenrePercentages": classifications_dict.values()})
